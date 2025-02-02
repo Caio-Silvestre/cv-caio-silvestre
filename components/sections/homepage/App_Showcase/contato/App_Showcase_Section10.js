a53ed8efcc2e869d0059582b9905dfa9f6d7a0f7
Caio S.G <caiosilvestre.dev@gmail.com>
2025-02-02 14:20:15 -0300

    fix-mobile-last-section

--- components/sections/homepage/App_Showcase/contato/App_Showcase_Section10.js
+++ components/sections/homepage/App_Showcase/contato/App_Showcase_Section10.js
@@ -8,52 +8,55 @@ export default function App_Showcase_Section10() {
       <section className="section-team section has-icon icon-bottom-left spdtb pt-5">
         <div className="animate__animated animate__fadeInDown">
           <div className="w-100 h-100 d-flex flex-column align-items-center justify-content-center">
-            <h2 className="heading-title size-l">Entre em contato</h2>
-            <div className="w-100 d-flex align-items-center justify-content-center">
-              <div className="w-50 pdl100">
+            <h2 className="heading-title wow animate__animated animate__fadeInDown w-75 text-center">
+              Entre em contato
+            </h2>
+            <div className="row w-100 d-flex align-items-center justify-content-center">
+              <div className="col-lg-6   wow animate__animated animate__fadeInLeft ">
                 <EnvioDeEmail className="pdl100 pdr100" />
               </div>
               <div
-                className="w-50 h-100 d-flex justify-content-end "
+                className="col-lg-6 wow animate__animated animate__fadeInRight  h-100 d-flex justify-content-center "
                 style={{ minHeight: "25rem" }}
               >
-                <div className="w-25 h-99 d-flex flex-column align-items-center justify-content-around ">
-                  <ZapZap />
-                  <Linkedin />
-                </div>
+                <div className="row">
+                  <div className="col-md-12  d-flex align-items-center justify-content-around ">
+                    <ZapZap />
+                    <Linkedin />
+                  </div>
+                  <div className=" content-btns col-md-12">
+                    <button
+                      className="button-cs"
+                      onClick={() => {
+                        const link = document.createElement("a");
+                        link.href = "/assets/docs/cv_caio_silvestre.pdf";
+                        link.download = "cv_caio_silvestre.pdf";
 
-                <div className=" content-btns">
-                  <button
-                    className="button-cs"
-                    onClick={() => {
-                      const link = document.createElement("a");
-                      link.href = "/assets/docs/cv_caio_silvestre.pdf";
-                      link.download = "cv_caio_silvestre.pdf";
+                        document.body.appendChild(link);
+                        link.click();
 
-                      document.body.appendChild(link);
-                      link.click();
+                        document.body.removeChild(link);
+                      }}
+                    >
+                      Baixar CV
+                    </button>
 
-                      document.body.removeChild(link);
-                    }}
-                  >
-                    Baixar CV
-                  </button>
+                    <button
+                      className="button-cs"
+                      onClick={() => {
+                        const link = document.createElement("a");
+                        link.href = "/assets/docs/cv_caio_silvestre.pdf";
+                        link.target = "_blank";
 
-                  <button
-                    className="button-cs"
-                    onClick={() => {
-                      const link = document.createElement("a");
-                      link.href = "/assets/docs/cv_caio_silvestre.pdf";
-                      link.target = "_blank";
+                        document.body.appendChild(link);
+                        link.click();
 
-                      document.body.appendChild(link);
-                      link.click();
-
-                      document.body.removeChild(link);
-                    }}
-                  >
-                    Abrir CV
-                  </button>
+                        document.body.removeChild(link);
+                      }}
+                    >
+                      Abrir CV
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
